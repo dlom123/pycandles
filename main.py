208555de66705ebe02c2f2c3010fa7045ecb021e
Daniel Lomelino <dlomelino@pm.me>
2021-03-14 12:13:45 -0400

    Draw candles at positions relative to one another

--- main.py
+++ main.py
@@ -10,21 +10,43 @@ EAST = 0
 WEST = 180
 PADDING = 5
 
+price_highest: int
+price_lowest: int
+current_market_price = 11.16
+
 
 def main():
+    # create candle objects and find price boundaries
+    candles = []
+    global price_highest, price_lowest
+    for i, day in enumerate(days):
+        candle = Candle(*day.values())
+        if i == 0 or candle.high > price_highest:
+            price_highest = candle.high
+        if i == 0 or candle.low < price_lowest:
+            price_lowest = candle.low
+        candles.append(candle)
+
+    high_low_diff = price_highest - price_lowest
+    pad_percentage = 0.1
+    pad_amount = high_low_diff * pad_percentage
+    bounds_top = round(price_highest + pad_amount, 1)
+    bounds_bottom = round(price_lowest - pad_amount, 1)
+
     # set up the window
     window_ = turtle.Screen()
     window_.setup(WINDOW_WIDTH, WINDOW_HEIGHT)
+    window_.setworldcoordinates(0, bounds_bottom, WINDOW_WIDTH, bounds_top)
     # create a turtle for drawing candles
     t = turtle.Turtle()
     t.speed(0)
     t.hideturtle()
+    t.penup()
 
-    candles = [Candle(*day.values()) for day in days]
+    # draw candles
     for i, candle in enumerate(candles):
+        t.setx((candle.width * i) + (PADDING * i))
         draw_candle(t, candle)
-        # prepare for next candle
-        t.forward(PADDING)
 
     turtle.done()
 
@@ -37,27 +59,26 @@ def draw_candle(
     draw_real_body(t, candle)
     if with_wicks:
         draw_wicks(t, candle)
-    else:
-        # send turtle to the bottom-right of the real body,
-        # pointing right to begin the next candle
-        t.setheading(EAST)
-        t.forward(candle.width)
 
 
 def draw_real_body(t: turtle.Turtle, candle: Candle) -> None:
     """
     Draws the real body of a candle from the bottom-left, counter-clockwise.
     """
+    t.penup()
+    t.sety(candle.real_body_bottom)
     t.fillcolor(candle.color)
     t.begin_fill()
+    t.setheading(EAST)
     t.forward(candle.width)
     t.setheading(NORTH)
-    t.forward(candle.height)
+    t.sety(candle.real_body_top)
     t.setheading(WEST)
     t.forward(candle.width)
     t.setheading(SOUTH)
-    t.forward(candle.height)
+    t.sety(candle.real_body_bottom)
     t.end_fill()
+    t.pendown()
 
 
 def draw_wicks(t: turtle.Turtle, candle: Candle) -> None:
@@ -69,29 +90,22 @@ def draw_wicks(t: turtle.Turtle, candle: Candle) -> None:
     t.penup()
     x, y = t.pos()
     # go to the top-center of the candle
-    t.goto(x + (candle.width // 2), y + candle.height)
+    t.goto(x + (candle.width // 2), candle.real_body_top)
     # point upwards
     t.setheading(NORTH)
     t.pendown()
-    t.forward(candle.wick_top_height)
+    t.sety(candle.high)
 
     # draw bottom wick
     t.penup()
     x, y = t.pos()
     # go to the bottom-center of the candle
-    t.goto(x, y - candle.wick_top_height - candle.height)
+    t.goto(x, candle.real_body_bottom)
     # point downwards
     t.setheading(SOUTH)
     t.pendown()
-    t.forward(candle.wick_bottom_height)
+    t.sety(candle.low)
     t.penup()
-    # go back up the bottom wick to the bottom of the real body and
-    # end at the bottom-right of the real body, pointing to the right
-    t.setheading(NORTH)
-    t.forward(candle.wick_bottom_height)
-    t.setheading(EAST)
-    t.forward(candle.width // 2)
-
 
 
 if __name__ == '__main__':
